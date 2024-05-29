95988eabd25b11c322c7073f3a52cd1e600154ed
Mingyun Son <mingyun.son@privatenote.co.kr>
2024-05-29 16:09:51 +0900

    get type 추가

--- src/statement.js
+++ src/statement.js
@@ -1,4 +1,52 @@
 export function statement(invoice, plays) {
+  const getAmount = (performance) => {
+    let amount;
+    const type = getTypeByPlayID(performance.playID);
+
+    switch (type) {
+      case 'tragedy':
+        amount = 40_000;
+
+        if (performance.audience > 30) {
+          amount += 1_000 * (performance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        amount = 30_000;
+
+        if (performance.audience > 20) {
+          amount += 10_000 + 500 * (performance.audience - 20);
+        }
+        amount += 300 * performance.audience;
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르: ${playType}`);
+    }
+
+    return amount;
+  };
+
+  const getCredit = (performance) => {
+    const type = getTypeByPlayID(performance.playID);
+
+    let credit = 0;
+
+    // 포인트를 적립한다.
+    credit += Math.max(performance.audience - 30, 0);
+
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ('comedy' === type) {
+      credit += Math.floor(performance.audience / 5);
+    }
+
+    return credit;
+  };
+
+  const getTypeByPlayID = (playID) => {
+    return plays[playID].type;
+  };
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
@@ -7,9 +55,9 @@ export function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    const thisAmount = getAmount(play.type, perf);
+    const thisAmount = getAmount(perf);
 
-    volumeCredits += getCredit(play.type, perf);
+    volumeCredits += getCredit(perf);
 
     // 청구 내역을 출력한다.
     result += `${play.name}: ${format(thisAmount / 100)} ${perf.audience}석\n`;
@@ -20,44 +68,3 @@ export function statement(invoice, plays) {
 
   return result;
 }
-
-const getAmount = (playType, performance) => {
-  let amount;
-
-  switch (playType) {
-    case 'tragedy':
-      amount = 40_000;
-
-      if (performance.audience > 30) {
-        amount += 1_000 * (performance.audience - 30);
-      }
-      break;
-    case 'comedy':
-      amount = 30_000;
-
-      if (performance.audience > 20) {
-        amount += 10_000 + 500 * (performance.audience - 20);
-      }
-      amount += 300 * performance.audience;
-      break;
-
-    default:
-      throw new Error(`알 수 없는 장르: ${playType}`);
-  }
-
-  return amount;
-};
-
-const getCredit = (playType, performance) => {
-  let credit = 0;
-
-  // 포인트를 적립한다.
-  credit += Math.max(performance.audience - 30, 0);
-
-  // 희극 관객 5명마다 추가 포인트를 제공한다.
-  if ('comedy' === playType) {
-    credit += Math.floor(performance.audience / 5);
-  }
-
-  return credit;
-};
