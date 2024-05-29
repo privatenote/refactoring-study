2763b08c4c648ddf7d1684f9801780a244281d5d
Hyeonjun Lee <guswns0528@gmail.com>
2024-05-29 15:29:09 +0900

    amountFor 함수 thisAmount를 result로 이름 변경

--- src/statement.js
+++ src/statement.js
@@ -27,29 +27,29 @@ export function statement(invoice, plays) {
   return result;
 
   function amountFor(perf, play) {
-    let thisAmount = 0;
+    let result = 0;
 
     switch (play.type) {
       case 'tragedy':
-        thisAmount = 40_000;
+        result = 40_000;
 
         if (perf.audience > 30) {
-          thisAmount += 1_000 * (perf.audience - 30);
+          result += 1_000 * (perf.audience - 30);
         }
         break;
       case 'comedy':
-        thisAmount = 30_000;
+        result = 30_000;
 
         if (perf.audience > 20) {
-          thisAmount += 10_000 + 500 * (perf.audience - 20);
+          result += 10_000 + 500 * (perf.audience - 20);
         }
-        thisAmount += 300 * perf.audience;
+        result += 300 * perf.audience;
         break;
 
       default:
         throw new Error(`알 수 없는 장르: ${play.type}`);
     }
 
-    return thisAmount;
+    return result;
   }
 }
