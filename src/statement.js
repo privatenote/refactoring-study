e5ceca1aa4146dc308553c9f5f3e7e3c7df47b9f
Hyeonjun Lee <guswns0528@gmail.com>
2024-05-29 16:10:20 +0900

    공연 정보 중간 데이터로 이동

--- src/statement.js
+++ src/statement.js
@@ -1,13 +1,14 @@
 export function statement(invoice, plays) {
   const statementData = {}
   statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
   return renderPlainText(statementData, invoice, plays)
 }
 
 function renderPlainText(data, invoice, plays) {
   let result = `청구내역 (고객명: ${data.customer})\n`;
 
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} ${perf.audience}석\n`;
   }
@@ -72,7 +73,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
 
@@ -81,7 +82,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalAmounts() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
 
