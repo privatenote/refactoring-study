93d203214c300014ac815c4db38c34e5f62ef04b
Hyeonjun Lee <guswns0528@gmail.com>
2024-05-29 16:16:51 +0900

    volumeCreditsFor 대신 중간 데이터 사용

--- src/statement.js
+++ src/statement.js
@@ -71,20 +71,6 @@ function renderPlainText(data) {
 
   return result;
 
-  function volumeCreditsFor(perf) {
-    let result = 0;
-
-    // 포인트를 적립한다.
-    result += Math.max(perf.audience - 30, 0);
-
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === perf.play.type) {
-      result += Math.floor(perf.audience / 5);
-    }
-
-    return result;
-  }
-
   function usd(amount) {
     const formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 })
       .format;
@@ -94,7 +80,7 @@ function renderPlainText(data) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
 
     return result;
