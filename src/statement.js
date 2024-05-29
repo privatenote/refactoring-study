0e54a34a3b6591c526e919c1316f6920f308a328
Hyeonjun Lee <guswns0528@gmail.com>
2024-05-29 15:36:06 +0900

    volumeCreditsFor 함수 추출

--- src/statement.js
+++ src/statement.js
@@ -9,13 +9,7 @@ export function statement(invoice, plays) {
     const play = playFor(perf);
     const thisAmount = amountFor(perf);
 
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === play.type) {
-      volumeCredits += Math.floor(perf.audience / 5);
-    }
+    volumeCredits += volumeCreditsFor(perf, play);
 
     // 청구 내역을 출력한다.
     result += `${play.name}: ${format(thisAmount / 100)} ${perf.audience}석\n`;
@@ -56,5 +50,19 @@ export function statement(invoice, plays) {
     }
 
     return result;
+  }
+
+  function volumeCreditsFor(perf, play) {
+    let volumeCredits = 0;
+
+    // 포인트를 적립한다.
+    volumeCredits += Math.max(perf.audience - 30, 0);
+
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ('comedy' === play.type) {
+      volumeCredits += Math.floor(perf.audience / 5);
+    }
+
+    return volumeCredits;
   }
 }
